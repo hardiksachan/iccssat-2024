7ffcfa25951cff5babe088e7ea12326117317bc3
Shivansh <sasthana083@gmail.com>
2023-03-31 19:16:10 +0530

    Committee Page Added

---
+++ app/committee/page.js
@@ -0,0 +1,221 @@
+import styles from "@/styles/pages/committee.module.scss"
+ const Committee=()=>
+ {
+    return(
+        
+        <div className={styles.CommitteeList}>
+            <div className={styles.heading}>Chief Patron</div>
+            <div className={styles.List}>Prof. (Dr) Ajay K. Sharma
+Director, National Institute of Technology, Delhi
+</div>
+<div className={styles.heading}>Steering Committee</div>
+<div className={styles.List}>Prof. Manoj Kumar
+Professor, Department of ECE, NIT Delhi
+
+</div>
+<div className={styles.List}>Dr. Harish Kumar
+Dean, Academics, NIT Delhi
+
+</div>
+<div className={styles.heading}>Executive General Chair</div>
+<div className={styles.List}>Prof. (Dr.) Geeta Sikka
+Professor & Head, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={styles.heading}>General Chair(s)</div>
+<div className={styles.List}>Dr. Anurag Singh
+Associate Professor, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={styles.heading}>Conference Chair(s)</div>
+<div className={styles.List}>Dr. Shelly Sachdeva
+Associate Professor, Dept of CSE, NIT Delhi
+</div>
+<div className={styles.heading}>Convenor(s)</div>
+<div className={styles.List}>Dr. Chandra Prakash
+Assistant Professor, Dept of CSE, NIT Delhi
+</div>
+<div className={styles.heading}>Secretary</div>
+<div className={styles.List}>Dr. Karan Verma
+Assistant Professor, Dept of CSE, NIT Delhi
+</div>
+<div className={styles.List}>Dr. Rishav Singh 
+Assistant Professor, Dept of CSE, NIT Delhi
+
+</div>
+<div className={styles.heading}>Finance Committee</div>
+<div className={`${styles.heading } ${styles.heading___small}`}>Coordinators</div>
+<div className={styles.List}>
+Prof. (Dr.) Geeta Sikka
+Professor & Head, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={styles.List}>
+Dr. Karan Verma
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={`${styles.heading } ${styles.heading___small}`}> Co-Coordinators</div>
+<div className={styles.List}>
+Dr. Shailendra Tripathi Assistant Professor, Dept of CSE, NIT Delhi
+
+</div>
+<div className={styles.heading}>Organizing Committee</div>
+<div className={styles.List}>
+Prof. (Dr.) Geeta Sikka
+Professor & Head, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={styles.List}>
+Dr. Shelly Sachdeva
+Associate Professor, Dept of CSE, NIT Delhi 
+
+
+</div>
+<div className={styles.List}>
+Dr. Anurag Singh
+Associate Professor, Dept of CSE, NIT Delhi
+
+</div>
+<div className={styles.List}>
+Dr. Karan Verma
+Assistant Professor, Dept of CSE, NIT Delhi
+
+</div>
+<div className={styles.List}>
+Dr. Chandra Prakash
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={styles.List}>
+Dr. Rishav Singh 
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+
+</div>
+<div className={`${styles.heading } ${styles.heading___small}`}> Co-Coordinators</div>
+<div className={styles.List}>
+Dr. Rohini Mahajan
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={styles.heading}>Publication Committee</div>
+<div className={`${styles.heading } ${styles.heading___small}`}>Coordinators</div>
+<div className={styles.List}>
+Dr. Anurag Singh
+Associate Professor, Dept of CSE, NIT Delhi
+
+
+
+</div>
+<div className={`${styles.heading } ${styles.heading___small}`}> Co-Coordinators</div>
+<div className={styles.List}>
+Dr. Divya Punia
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={styles.List}>
+Dr. Karan Verma
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={styles.List}>
+Dr. Chandra Prakash
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={styles.heading}>Publicity Committee</div>
+<div className={`${styles.heading } ${styles.heading___small}`}>Coordinators</div>
+<div className={styles.List}>
+Dr. Shelly Sachdeva
+Associate Professor, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={`${styles.heading } ${styles.heading___small}`}> Co-Coordinators</div>
+<div className={styles.List}>
+Dr. Rishav Singh 
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+
+</div>
+<div className={styles.List}>
+Dr. Prabhakar Agarwal
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+</div>
+<div className={styles.heading}>Hospitality and Accommodation Committee</div>
+<div className={`${styles.heading } ${styles.heading___small}`}>Coordinators</div>
+<div className={styles.List}>
+Dr. Karan Verma
+Associate Professor, Dept of CSE, NIT Delhi
+
+
+
+</div>
+<div className={`${styles.heading } ${styles.heading___small}`}> Co-Coordinators</div>
+<div className={styles.List}>
+Dr. Jaspinder Kour
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+
+</div>
+<div className={styles.heading}>Transportation Committee</div>
+<div className={`${styles.heading } ${styles.heading___small}`}>Coordinators</div>
+<div className={styles.List}>
+Dr. Rishav Singh 
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+
+
+</div>
+<div className={`${styles.heading } ${styles.heading___small}`}> Co-Coordinators</div>
+<div className={styles.List}>
+
+Dr. Shailendra Tripathi
+Assistant Professor, Dept of CSE, NIT Delhi
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+        
+
+        
+
+    )
+ }
+ export default Committee
