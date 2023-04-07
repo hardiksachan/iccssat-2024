b0d472a1e0213d9e0eeb38c095a0fd486db803d7
Hardik <sachanhardik@gmail.com>
2023-04-07 12:26:55 +0530

    update images for committe members

--- app/committee.js
+++ app/committee.js
@@ -1,95 +1,103 @@
-import img1 from "@/assets/img/tracks-intro-img-01.jpg"
+import anuragSingh from "@/assets/committee-members/anurag-singh.jpg";
+import chandraPrakash from "@/assets/committee-members/chandra-prakash.jpg";
+import director from "@/assets/committee-members/director.jpg";
+import geetaSikka from "@/assets/committee-members/geeta-sikka.jpg";
+import harishKumar from "@/assets/committee-members/harish-kumar.jpg";
+import karanVerma from "@/assets/committee-members/karan-verma.jpg";
+import rishavSingh from "@/assets/committee-members/rishav-singh.jpg";
+import shellySachdeva from "@/assets/committee-members/shelly-sachdeva.png";
+import img1 from "@/assets/img/tracks-intro-img-01.jpg";
 
 const committee = [
-    {
-        title: "Chief Patron",
-        members: [
-            {
-                name: "Prof. (Dr) Ajay K. Sharma",
-                designation: "Director",
-                affiliation: "National Institute of Technology, Delhi",
-                img: img1,
-            }
-        ]
-    },
-    {
-        title: "Executive General Chair",
-        members: [
-            {
-                name: "Prof. (Dr.) Geeta Sikka",
-                designation: "Professor & Head, Dept of CSE",
-                affiliation: "National Institute of Technology, Delhi",
-                img: img1,
-            },
-        ]
-    },
-    {
-        title: "General Chairs",
-        members: [
-            {
-                name: "Dr. Anurag Singh",
-                designation: "Associate Professor, Dept of CSE",
-                affiliation: "National Institute of Technology, Delhi",
-                img: img1,
-            },
-        ]
-    },
-    {
-        title: "Conference Chairs",
-        members: [
-            {
-                name: "Dr. Shelly Sachdeva",
-                designation: "Associate Professor, Dept of CSE",
-                affiliation: "National Institute of Technology, Delhi",
-                img: img1,
-            },
-        ]
-    },
-    {
-        title: "Steering Committee",
-        members: [
-            {
-                name: "Prof. Manoj Kumar",
-                designation: "Professor, Department of ECE",
-                affiliation: "National Institute of Technology, Delhi",
-                img: img1,
-            },
-            {
-                name: "Dr. Harish Kumar",
-                designation: "Dean, Academics",
-                affiliation: "National Institute of Technology, Delhi",
-                img: img1,
-            }
-        ]
-    },
-    {
-        title: "Conveners",
-        members: [
-            {
-                name: "Dr. Chandra Prakash",
-                designation: "Assistant Professor, Dept of CSE",
-                affiliation: "National Institute of Technology, Delhi",
-                img: img1,
-            },
-        ]
-    },
-    {
-        title: "Secretary",
-        members: [
-            {
-                name: "Dr. Karan Verma",
-                designation: "Assistant Professor, Dept of CSE",
-                affiliation: "National Institute of Technology, Delhi",
-                img: img1,
-            },
-            {
-                name: "Dr. Rishav Singh",
-                designation: "Assistant Professor, Dept of CSE",
-                affiliation: "National Institute of Technology, Delhi",
-                img: img1,
-            },
-        ]
-    }
-]
+  {
+    title: "Chief Patron",
+    members: [
+      {
+        name: "Prof. (Dr) Ajay K. Sharma",
+        designation: "Director",
+        affiliation: "National Institute of Technology, Delhi",
+        img: director,
+      },
+    ],
+  },
+  {
+    title: "Executive General Chair",
+    members: [
+      {
+        name: "Prof. (Dr.) Geeta Sikka",
+        designation: "Professor & Head, Dept of CSE",
+        affiliation: "National Institute of Technology, Delhi",
+        img: geetaSikka,
+      },
+    ],
+  },
+  {
+    title: "General Chairs",
+    members: [
+      {
+        name: "Dr. Anurag Singh",
+        designation: "Associate Professor, Dept of CSE",
+        affiliation: "National Institute of Technology, Delhi",
+        img: anuragSingh,
+      },
+    ],
+  },
+  {
+    title: "Conference Chairs",
+    members: [
+      {
+        name: "Dr. Shelly Sachdeva",
+        designation: "Associate Professor, Dept of CSE",
+        affiliation: "National Institute of Technology, Delhi",
+        img: shellySachdeva,
+      },
+    ],
+  },
+  {
+    title: "Steering Committee",
+    members: [
+      {
+        name: "Prof. Manoj Kumar",
+        designation: "Professor, Department of ECE",
+        affiliation: "National Institute of Technology, Delhi",
+        img: img1,
+      },
+      {
+        name: "Dr. Harish Kumar",
+        designation: "Dean, Academics",
+        affiliation: "National Institute of Technology, Delhi",
+        img: harishKumar,
+      },
+    ],
+  },
+  {
+    title: "Conveners",
+    members: [
+      {
+        name: "Dr. Chandra Prakash",
+        designation: "Assistant Professor, Dept of CSE",
+        affiliation: "National Institute of Technology, Delhi",
+        img: chandraPrakash,
+      },
+    ],
+  },
+  {
+    title: "Secretary",
+    members: [
+      {
+        name: "Dr. Karan Verma",
+        designation: "Assistant Professor, Dept of CSE",
+        affiliation: "National Institute of Technology, Delhi",
+        img: karanVerma,
+      },
+      {
+        name: "Dr. Rishav Singh",
+        designation: "Assistant Professor, Dept of CSE",
+        affiliation: "National Institute of Technology, Delhi",
+        img: rishavSingh,
+      },
+    ],
+  },
+];
 
-export default committee
+export default committee;
